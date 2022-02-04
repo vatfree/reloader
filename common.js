160e656dadab29ee62020b0ba81bb3fb9dddb864
Tom Tilley <Tominator2@users.noreply.github.com>
2022-02-04 11:21:18 +1030

    Update package name for settings check

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 import { getSettings } from 'meteor/quave:settings';
 
-export const PACKAGE_NAME = 'quave:reloader';
+export const PACKAGE_NAME = 'jaskinn:reloader';
 export const settings = getSettings({ packageName: PACKAGE_NAME });
 
 export const debugFn = (message, context) => {
