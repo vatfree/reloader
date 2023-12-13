fba63cf670c66181c0419cb82db3695cc347b484
Jaco Swarts <jaco@vatfree.com>
2023-12-13 17:44:49 +0200

    remove pinned launch-screen version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jaskinn:reloader',
-  version: '2.0.4',
+  version: '2.0.5',
   summary: 'More control over hot code push reloading',
   git: 'https://github.com/jaskinn/reloader/',
 });
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.10.2');
 
   api.use(
-    ['ecmascript', 'reload', 'reactive-var', 'tracker', 'launch-screen@2.0.0-rc2140.1'],
+    ['ecmascript', 'reload', 'reactive-var', 'tracker', 'launch-screen'],
     'client'
   );
 
